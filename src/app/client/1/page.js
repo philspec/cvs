ca525d0382fe02b28d4246023a1fb63d1a571839
phillips <qwertysonata@gmail.com>
2024-12-16 20:23:05 +0530

    ok

--- src/app/client/1/page.js
+++ src/app/client/1/page.js
@@ -1,19 +1,26 @@
 "use client"
- 
-import { useState, useEffect } from 'react';
+
+import { useState, useEffect, useMemo } from 'react';
 
 export default function ClientComponent() {
     const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
+    const memoizedPosts = useMemo(() => {
         async function fetchData() {
             const res = await fetch('https://jsonplaceholder.typicode.com/posts');
             const data = await res.json();
             setPosts(data);
         }
         fetchData();
+        return posts;
     }, []);
-    console.log("client");
+
+    useEffect(() => {
+        if (memoizedPosts && memoizedPosts.length > 0) {
+            setPosts(memoizedPosts);
+        }
+    }, [memoizedPosts]);
+
     return (
         <div>
             {posts.map(post => (
--- src/app/server/1/page.js
+++ src/app/server/1/page.js
@@ -1,26 +1,7 @@
-"use client"
-
-import { useState, useEffect, useMemo } from 'react';
-
-export default function ClientComponent() {
-    const [posts, setPosts] = useState([]);
-
-    const memoizedPosts = useMemo(() => {
-        async function fetchData() {
-            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const data = await res.json();
-            setPosts(data);
-        }
-        fetchData();
-        return posts;
-    }, []);
-
-    useEffect(() => {
-        if (memoizedPosts && memoizedPosts.length > 0) {
-            setPosts(memoizedPosts);
-        }
-    }, [memoizedPosts]);
-
+export default async function ServerComponent() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await res.json();
+    console.log("server");
     return (
         <div>
             {posts.map(post => (
