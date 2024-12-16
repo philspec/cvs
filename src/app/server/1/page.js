b5373ed328d147908cd46f40fb45c37868293e8e
phillips <qwertysonata@gmail.com>
2024-12-16 20:17:16 +0530

    ok

--- src/app/server/1/page.js
+++ src/app/server/1/page.js
@@ -1,7 +1,26 @@
-export default async function ServerComponent() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const posts = await res.json();
-    console.log("server");
+"use client"
+
+import { useState, useEffect, useMemo } from 'react';
+
+export default function ClientComponent() {
+    const [posts, setPosts] = useState([]);
+
+    const memoizedPosts = useMemo(() => {
+        async function fetchData() {
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await res.json();
+            setPosts(data);
+        }
+        fetchData();
+        return posts;
+    }, []);
+
+    useEffect(() => {
+        if (memoizedPosts && memoizedPosts.length > 0) {
+            setPosts(memoizedPosts);
+        }
+    }, [memoizedPosts]);
+
     return (
         <div>
             {posts.map(post => (
