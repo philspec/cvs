a8af3fb5095bbdab40c7661ca88e1e8b62da49fd
phillips <qwertysonata@gmail.com>
2024-12-17 00:33:26 +0530

    ok

--- src/app/client/1/page.js
+++ src/app/client/1/page.js
@@ -1,31 +1,9 @@
 "use client"
 
-import { useState, useEffect, useMemo } from 'react';
-
 export default function ClientComponent() {
-    const [posts, setPosts] = useState([]);
-
-    const memoizedPosts = useMemo(() => {
-        async function fetchData() {
-            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const data = await res.json();
-            setPosts(data);
-        }
-        fetchData();
-        return posts;
-    }, []);
-
-    useEffect(() => {
-        if (memoizedPosts && memoizedPosts.length > 0) {
-            setPosts(memoizedPosts);
-        }
-    }, [memoizedPosts]);
-
     return (
         <div>
-            {posts.map(post => (
-                <div key={post.id}>{post.title}</div>
-            ))}
+          Client
         </div>
     );
 }
--- src/app/server/1/page.js
+++ src/app/server/1/page.js
@@ -1,16 +1,10 @@
 export default async function ServerComponent() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const posts = await res.json();
-    console.log("server");
-    return <PostsDisplay posts={posts} />;
-}
+    
+ 
 
-function PostsDisplay({ posts }) {
     return (
         <div>
-            {posts.map(post => (
-                <div key={post.id}>{post.title}</div>
-            ))}
+           server
         </div>
     );
 }
