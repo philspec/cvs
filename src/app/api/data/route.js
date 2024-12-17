5630793f57c88c56d27df10478e8c051dafadce8
phillips <qwertysonata@gmail.com>
2024-12-17 20:35:25 +0530

    ok

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -1,6 +1,7 @@
 export async function GET(request) {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await res.json();
+    console.log("data is fetched");
   
     return Response.json({ data })
   }
